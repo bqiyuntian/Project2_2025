98bdd2caabec66a43f8c0f454249eed42bb2cbe1
bai <17605229265@163.com>
2025-04-15 20:20:13 +0800

    fixes

--- SoilSensor.py
+++ SoilSensor.py
@@ -9,20 +9,21 @@ GPIO.setup(channel, GPIO.IN)
 
 def callback(channel):
         if GPIO.input(channel):
-                 print  ("Water Detected!")
+                print  ("Water Detected!")
         else :
-                 print  ("No Water Detected!")
+                print  ("No Water Detected!")
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300) # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback) # assign function to GPIO PIN, Run function on change
+GPIO.add_event_callback(channel, callback)
+ # assign function to GPIO PIN, Run function on change
 
 # infinite loop
 try:
     while True:
         if GPIO.input(channel):
-            print ("soil is wet")
-        else:
             print ("soil is dry")
+        else:
+            print ("soil is wet")
         time.sleep(10800)
 except KeyboardInterrupt:
     GPIO.cleanup()
