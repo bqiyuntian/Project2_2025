4ae45e50921e9d43d6a4607ab68caab44d98262a
bai <17605229265@163.com>
2025-04-22 20:10:52 +0800

    maybe the final version

--- SoilSensorEmail.py
+++ SoilSensorEmail.py
@@ -11,58 +11,55 @@ from_email_addr = "17605229265@163.com"
 from_email_pass = "QSPWa32LkjeZ25kL"
 to_email_addr = "17605229265@163.com"
 
-lastValue = -1
-lastTime = 0
+last_send_time = 0
 
 def callback(channel) :
-    global lastValue
+  
     if GPIO.input(channel) :
         print ("water detected!")
-        lastValue = 1
+     
     else :
         print ("water not detected!")
-        lastValue = 0
+       
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime = 300)
 GPIO.add_event_callback(channel, callback)
 
 def send_email(subject, body) :
-    server = smtplib.SMTP('smtp.163.com', 25)
-    server.starttls()
-    server.login(from_email_addr, from_email_pass)
+    global last_send_time
+    current_time = time.time()
+    
+    if current_time - last_sent_time >= 6*60*60:
+        server = smtplib.SMTP('smtp.163.com', 25)
+        server.starttls()
+        server.login(from_email_addr, from_email_pass)
    
-    msg = EmailMessage()
-    msg.set_content(body)
-    msg['Subject'] = subject
-    msg['From'] = from_email_addr
-    msg['To'] = to_email_addr
+        msg = EmailMessage()
+        msg.set_content(body)
+        msg['Subject'] = subject
+        msg['From'] = from_email_addr
+        msg['To'] = to_email_addr
 
-    server.send_message(msg)
-    server.quit()    
-    print ("email sent")
-
+        server.send_message(msg)
+        server.quit()    
+        print ("email sent")
+        last_send_time = current_time
+    else:
+       print("time interval not reached, email not sent")
 def main() :
-    global lastTime, lastValue
     while True:
         current_time = time.localtime()
         current_hour = current_time.tm_hour + 8
         print ("current time hour:", current_hour)
      
-        if current_hour != lastTime:
-            print ("time to send the first email of the day")
-            send_email("test email", "hello from raspberry pi")
-            lastTime = current_hour
-  
-        difference = current_hour - lastValue
-        if difference > 3:
-            print("time difference > 3. time to send an email")
-            send_email("water needed", "please water your plant.")
-            lastValue = current_hour
+        time.sleep(60*60)
 
-        else:
-            print("hour difference < 4.")
-            send_email("waater don't needed")
-
-        time.sleep(60)
+        if current_hour % 6 == 0:
+            current_value = GPIO.input(channel)
+            status = "watter detected" if current_value else "water not detscted"
+            subject = "soil sensor update"
+            body =f"hello,the soil sensor status is: {status}"
+            send_email(subject, body) 
+           
 
 if __name__== "__main__":
     main()
